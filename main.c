418feefae402dcdaa3156788c782d114be7e107e
KatliKoma <katlegokoma1@gmail.com>
2024-01-18 12:14:41 +0200

    first commit

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include "monty.h"
-
-extern interpreter_t interpreter;
-
+interpreter_t interpreter = {NULL, NULL, NULL, 0};
 /**
 * main - Monty code interpreter entry point
 * @argc: number of command line arguments
@@ -13,7 +11,7 @@ int main(int argc, char *argv[])
 	char *file_content;
 	FILE *file_pointer;
 	size_t buffer_size = 0;
-	char *read_line = NULL;
+	ssize_t read_line = 1;
 	stack_t *data_stack = NULL;
 	unsigned int line_counter = 0;
 
@@ -24,29 +22,25 @@ int main(int argc, char *argv[])
 	}
 	file_pointer = fopen(argv[1], "r");
 	interpreter.file = file_pointer;
-
 	if (!file_pointer)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	while (read_line != NULL)
+	while (read_line > 0)
 	{
-		file_content = malloc(buffer_size);
-		read_line = fgets(file_content, buffer_size, file_pointer);
+		file_content = NULL;
+		read_line = getline(&file_content, &buffer_size, file_pointer);
 		interpreter.content = file_content;
 		line_counter++;
-
-		if (read_line != NULL)
+		if (read_line > 0)
 		{
 			execute(file_content, &data_stack, line_counter, file_pointer);
 		}
-
 		free(file_content);
 	}
-
 	free_stack(data_stack);
 	fclose(file_pointer);
 	return (0);
 }
+

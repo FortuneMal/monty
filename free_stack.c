d9cc69b5be1511b6dac904874ebf648b8449bed1
KatliKoma <katlegokoma1@gmail.com>
2024-01-18 11:23:23 +0200

    first commit

---
+++ free_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * free_stack - Frees the memory allocated for the stack
+ * @stack: Pointer to the top of the stack
+ */
+void free_stack(stack_t *stack)
+{
+    stack_t *current = stack;
+    stack_t *next_node;
+
+    while (current != NULL)
+    {
+        next_node = current->next;
+        free(current);
+        current = next_node;
+    }
+}
--- main.c
+++ main.c
@@ -1,51 +1,46 @@
-#define _POSIX_C_SOURCE 200809L
-
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include "monty.h"
 interpreter_t interpreter = {NULL, NULL, NULL, 0};
+
 /**
-* monty_interpreter - Entry point for the Monty code interpreter
-* @argument_count: number of command line arguments
-* @argument_values: Monty file location
+* main - Monty code interpreter entry point
+* @argc: number of command line arguments
+* @argv: array of command line arguments
 * Return: 0 on success
 */
-int monty_interpreter(int argument_count, char *argument_values[])
+int main(int argc, char *argv[])
 {
 	char *file_content;
 	FILE *file_pointer;
 	size_t buffer_size = 0;
-	size_t read_line = 1;
+	char *read_line = NULL;
 	stack_t *data_stack = NULL;
 	unsigned int line_counter = 0;
 
-	if (argument_count != 2)
-	{
-		fprintf(stderr, "USAGE: %s file\n", argument_values[0]);
-		exit(EXIT_FAILURE);
-	}
-	file_pointer = fopen(argument_values[1], "r");
-	interpreter.file = file_pointer;
-	if (!file_pointer)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argument_values[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		file_content = NULL;
-		read_line = getline(&file_content, &buffer_size, file_pointer);
-		interpreter.content = file_content;
-		line_counter++;
-		if (read_line > 0)
-		{
-			execute(file_content, &data_stack, line_counter, file_pointer);
-		}
-		free(file_content);
-	}
-	free_stack(data_stack);
-	fclose(file_pointer);
-	return (0);
+   if (argc != 2)
+   {
+       fprintf(stderr, "USAGE: %s file\n", argv[0]);
+       exit(EXIT_FAILURE);
+   }
+   file_pointer = fopen(argv[1], "r");
+   interpreter.file = file_pointer;
+   if (!file_pointer)
+   {
+       fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+       exit(EXIT_FAILURE);
+   }
+   while (read_line != NULL)
+   {
+	   file_content = malloc(buffer_size);
+	   read_line = fgets(file_content, buffer_size, file_pointer);
+	   interpreter.content = file_content;
+	   line_counter++;
+	   if (read_line != NULL)
+	   {
+		   execute(file_content, &data_stack, line_counter, file_pointer);
+	   }
+       free(file_content);
+   }
+   free_stack(data_stack);
+   fclose(file_pointer);
+   return (0);
 }

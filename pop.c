643e1b50996a83438065f6b4b6aa3e9b0a7e075b
FortuneMal <fmlaza512@gmail.com>
2024-01-18 06:41:30 -0800

    lol

---
+++ pop.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * pop - Removes the top element of the stack
+ * @stack: Pointer to the top of the stack
+ * @line_number: Current line number in the bytecode file
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	
+	stack_t *top = *stack;
+	*stack = top->next;
+	free(top);
+}
+
